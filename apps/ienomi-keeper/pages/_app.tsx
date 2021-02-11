1e9a6813cf8083566408b0a8afe4210144790b4e
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-02-11 21:56:06 +0900

    chore: setup workspace and coding guide

--- apps/ienomi-keeper/pages/_app.tsx
+++ apps/ienomi-keeper/pages/_app.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to ienomi-keeper!</title>
+        <title>Ienomi Keeper</title>
       </Head>
       <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to ienomi-keeper!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
       </div>
     </>
   );
