9f0e470ed86e14fd1469f360446c7baacf836cf4
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-02-17 09:34:45 +0900

    feat: setup fireabase front

--- apps/ienomi-keeper/pages/login/index.tsx
+++ apps/ienomi-keeper/pages/login/index.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SessionRepository } from '@ienomi/repository';
+import { router } from 'next/client';
+import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../context/Auth';
+
+type FormType = { email: string; password: string };
 
 const Login = () => {
-  return <div>Login</div>;
+  const auth = useContext(AuthContext);
+
+  const { register, handleSubmit } = useForm<FormType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = ({ email, password }: FormType) => {
+    SessionRepository.login({ email, password });
+  };
+
+  if (auth?.uid) {
+    router.replace('/');
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <input type={'text'} name={'email'} ref={register} />
+        </div>
+        <div>
+          <input type={'password'} name={'password'} ref={register} />
+        </div>
+        <div>
+          <button onClick={handleSubmit(onSubmit)}>ログイン</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
