efa8e1143fe4f26f8a157429e772e389a93fe157
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-02-18 09:18:54 +0900

    feat: setup login form

--- apps/ienomi-keeper/pages/login/index.tsx
+++ apps/ienomi-keeper/pages/login/index.tsx
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { SessionRepository } from '@ienomi/repository';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
+import { object, string } from 'yup';
 import { AuthContext } from '../../context/Auth';
 
 type FormType = { email: string; password: string };
@@ -10,7 +12,12 @@ const Login = () => {
   const router = useRouter();
   const auth = useContext(AuthContext);
 
-  const { register, handleSubmit } = useForm<FormType>({
+  const schema = object().shape({
+    email: string().required('メールアドレスを入力してください'),
+    password: string().required('パスワードを入力してください'),
+  });
+  const { register, handleSubmit, setError, errors, formState } = useForm<FormType>({
+    resolver: yupResolver(schema),
     defaultValues: {
       email: '',
       password: '',
@@ -21,8 +28,19 @@ const Login = () => {
     auth?.uid && router.replace('/');
   }, [auth, router]);
 
-  const onSubmit = ({ email, password }: FormType) => {
-    SessionRepository.login({ email, password });
+  const onSubmit = async ({ email, password }: FormType) => {
+    try {
+      await SessionRepository.login({ email, password });
+    } catch (error) {
+      setError('email', {
+        type: 'auth',
+        message: 'メールアドレスまたはパスワードが正しくありません',
+      });
+      setError('password', {
+        type: 'auth',
+        message: 'メールアドレスまたはパスワードが正しくありません',
+      });
+    }
   };
 
   return (
@@ -31,12 +49,16 @@ const Login = () => {
       <form>
         <div>
           <input type={'text'} name={'email'} ref={register} />
+          <p>{errors?.email?.message}</p>
         </div>
         <div>
           <input type={'password'} name={'password'} ref={register} />
+          <p>{errors?.password?.message}</p>
         </div>
         <div>
-          <button onClick={handleSubmit(onSubmit)}>ログイン</button>
+          <button onClick={handleSubmit(onSubmit)} disabled={formState.isSubmitting}>
+            ログイン
+          </button>
         </div>
       </form>
     </div>
