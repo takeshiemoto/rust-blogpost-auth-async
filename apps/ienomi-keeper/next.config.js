c87a209a87b6e12924212dff82e28824a873f526
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-03-08 16:51:34 +0900

    feat: setup rsp

--- apps/ienomi-keeper/next.config.js
+++ apps/ienomi-keeper/next.config.js
@@ -1,13 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable */
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withPlugins = require('next-compose-plugins');
+const withCSS = require('@zeit/next-css');
+const withTM = require('next-transpile-modules')([
+  '@adobe/react-spectrum',
+  '@spectrum-icons/.*',
+  '@react-spectrum/.*',
+]);
 
-module.exports = withNx({
-  env: {
-    FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  },
-});
+module.exports = withPlugins(
+  [withCSS, withTM],
+  withNx({
+    env: {
+      FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      FIREBASE_MESSAGING_SENDER_ID:
+        process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    },
+  })
+);
--- apps/ienomi-keeper/pages/_app.tsx
+++ apps/ienomi-keeper/pages/_app.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { AppProps } from 'next/app';
+import { SSRProvider, Provider, defaultTheme } from '@adobe/react-spectrum';
 
+import { AppProps } from 'next/app';
 import './styles.css';
 import { AuthProvider } from '../domain/app/AuthContext';
 import { SingUpProvider } from '../domain/singup/SignupContext';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <SingUpProvider>
-        <Component {...pageProps} />
-      </SingUpProvider>
-    </AuthProvider>
+    <SSRProvider>
+      <Provider theme={defaultTheme}>
+        <AuthProvider>
+          <SingUpProvider>
+            <Component {...pageProps} />
+          </SingUpProvider>
+        </AuthProvider>
+      </Provider>
+    </SSRProvider>
   );
 }
 
